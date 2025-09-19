1b49a08fb11ea3652b1eb363b7f5f151a214a762
ralphreevencarandang <rvncrndng@gmail.com>
2025-09-19 11:15:33 +0800

    Implemenet Art Section

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Cocktails from "./components/Cocktails"
 import About from "./components/About"
-import { useGSAP } from "@gsap/react"
+import Art from "./components/Art"
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
 
@@ -19,6 +19,7 @@ function App() {
       <Hero/>
       <Cocktails/>
       <About/>
+      <Art/>
  
     </main>
   )
